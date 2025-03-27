0c398067f7bc0fa62edbff4d32252e029bbbfbcc
zakariyamistura <misturazakariya@gmail.com>
2025-03-27 11:54:16 -0700

    add landing

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render, redirect
 
 
 def land(request):
-    posts = posts
+    posts = Blog.objects.all() 
     return render(request, 'landing.html', {'posts': posts})
 
 @login_required
