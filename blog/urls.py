e73cc8e68ca3f800dfb8bc72d277d548c5e8f1e6
zakariyamistura <misturazakariya@gmail.com>
2025-03-27 10:56:22 -0700

    add landing

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import main, detail, create_note, delete_nnote, edit_note, login_user, create_user, logout_user
+from .views import main, detail, create_note, delete_nnote, edit_note, login_user, create_user, logout_user, land
 
 
 urlpatterns = [
+    path("", land, name='Landing page'),
     path("home/", main, name="main"),
     path("<int:note_id>/", detail, name="detail"),
-    path("", login_user, name="login_user"),
+    path("login", login_user, name="login_user"),
     path("signup/", create_user, name="create_user"),
     path("delete/<int:note_id>/", delete_nnote, name="delete"),
     path("create/", create_note, name="create_note"),
